f1f70c1be53f2a942e6c4df6f773265fd8ca9667
Gabriel Rogan <gabriel@MacBook-Pro-5.local>
2018-04-05 14:58:25 +0200

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -5,73 +5,61 @@ import Utils from './utils';
 import Version from './version';
 import Share from './share';
 
-const PRISMIC_ENDPOINT = window.prismic && window.prismic.endpoint;
-let START_EXPERIMENT = () => {};
 
-const config = (() => {
-  const matches = PRISMIC_ENDPOINT && PRISMIC_ENDPOINT.match(new RegExp('(https?://([^/]*))'));
-  if (matches && matches[1] && matches[2]) {
-    const baseURL = matches[1].replace(/\.cdn\.prismic\.io/, '.prismic.io');
-    const editorTab = matches[2].replace(/\.cdn\.prismic\.io/, '.prismic.io');
-    const location = {
+// window.prismic setup
+let _startExperiment = null;
+const setup = Utils.debounce(() => Toolbar.setup(), 500, true);
+const setupEditButton = _ => config && EditBtn.setup(config);
+const startExperiment = expId => { if (expId) _startExperiment = _ => Experiments.start(expId) };
+const version = Version.value;
+const endpoint = window.prismic && window.prismic.endpoint.replace(/\.cdn\.prismic\.io/, '.prismic.io');
+
+window.prismic = { setup, setupEditButton, startExperiment, version, endpoint };
+
+
+// config
+const config = (_ => {
+
+  if (!endpoint) return null;
+
+  const urlComponents = endpoint.match(new RegExp('https?://([^/]*)'));
+
+  if (!urlComponents[1]) return null;
+
+  return {
+    baseURL: urlComponents[0], // "http://foyer-demo.wroom.test"
+    editorTab: urlComponents[1], // "foyer-demo.wroom.test"
+    location: {
       origin: window.location.origin,
-      hash: window.location.hash,
-      pathname: window.location.pathname,
+      hash: window.location.hash, // "http://localhost:3000"
+      pathname: window.location.pathname, // "/fr-lu/page/foyer-voyage"
       search: window.location.search,
-    };
-    return { baseURL, editorTab, location };
+    }
   }
-  return null;
+
 })();
 
-const setupToolbar = Utils.debounce(() => Toolbar.setup(), 500, true);
 
-function setupEditButton() {
-  if (config) {
-    EditBtn.setup(config);
-  }
-}
+// session iFrame
+const iframe = document.createElement('iframe');
+iframe.src = `${config.baseURL}/previews/session/get`;
+iframe.style.display = 'none';
+setTimeout(_ => document.body.appendChild(iframe), 0); // script ran before body rendered
 
-function startExperiment(expId) {
-  if (expId) {
-    START_EXPERIMENT = () => Experiments.start(expId);
-  }
-}
 
 // listen to session iframe
 window.addEventListener('message', e => {
-  if (e.data.type === 'preview') {
-    window.prismicSession = e.data.data
-    if (config) {
-      Share.listen(config, () => {
-        START_EXPERIMENT();
-        setupToolbar();
-        setupEditButton();
-      });
-    }
-  }
+
+  if (e.data.type !== 'preview') return;
+
+  window.prismicSession = e.data.data;
+
+  if (!config) return;
+
+  Share.listen(config, _ => {
+    _startExperiment && _startExperiment();
+    setup();
+    setupEditButton();
+  });
+
 })
-
-// session iframe
-const parser = document.createElement('a');
-parser.href = PRISMIC_ENDPOINT;
-const iframe = document.createElement('iframe');
-iframe.src = `${parser.protocol}//${parser.host}/previews/session/get`;
-iframe.style.display = 'none';
-setTimeout(_ => {
-  document.body.appendChild(iframe);
-}, 0);
-
-exports.setup = setupToolbar;
-exports.setupEditButton = setupEditButton;
-exports.startExperiment = startExperiment;
-exports.version = Version.value;
-exports.endpoint = PRISMIC_ENDPOINT;
-
-window.prismic = {
-  setup: setupToolbar,
-  startExperiment,
-  setupEditButton,
-  version: Version.value,
-  endpoint: PRISMIC_ENDPOINT,
-};
