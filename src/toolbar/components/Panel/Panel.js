def4161caaaf4a6668614fd69f529398779ab49d
Gabriel Rogan <rogangabe@gmail.com>
2018-11-28 17:25:16 +0100

    amend last animation fix

--- src/toolbar/components/Panel/Panel.js
+++ src/toolbar/components/Panel/Panel.js
@@ -1,15 +1,20 @@
+import { TransitionGroup } from 'react-transition-group';
 import { switchy } from 'common';
 import { views, Animation } from '..';
 import { DocumentPanel, PreviewPanel, SharePanel } from '.';
 
 const { DOCS, DRAFTS, SHARE } = views;
 
-export const Panel = ({ closePanel, documents, preview, page, onDocumentClick }) => !page || (
-  <Animation.SlideIn>{
-    switchy(page)({
-      [DOCS]: <DocumentPanel documents={documents} onClose={closePanel} onDocumentClick={onDocumentClick} />,
-      [DRAFTS]: <PreviewPanel preview={preview} onClose={closePanel} />,
-      [SHARE]: <SharePanel preview={preview} onClose={closePanel} />,
-    })
-  }</Animation.SlideIn> 
+export const Panel = ({ closePanel, documents, preview, page, onDocumentClick }) => (
+  <TransitionGroup>{
+    !page || (
+      <Animation.SlideIn key={1}>{
+        switchy(page)({
+          [DOCS]: <DocumentPanel documents={documents} onClose={closePanel} onDocumentClick={onDocumentClick} />,
+          [DRAFTS]: <PreviewPanel preview={preview} onClose={closePanel} />,
+          [SHARE]: <SharePanel preview={preview} onClose={closePanel} />,
+        })
+      }</Animation.SlideIn> 
+    )
+  }</TransitionGroup>
 )
