8f9a18c8d78a686360295f5add74f7d7b195b80c
Gabriel Rogan <rogangabe@gmail.com>
2019-02-20 18:13:07 +0100

    unencode

--- src/iframe/preview.js
+++ src/iframe/preview.js
@@ -28,7 +28,7 @@ export const closePreview = _ => demolishCookie('io.prismic.previewSession');
 // Share
 export const sharePreview = memoize(async location => {
   const imageId = location.pathname.slice(1) + location.hash + sessionId + '.jpg';
-  const imageName = encodeURIComponent(imageId); // check this
+  const imageName = imageId
   const session = await getShareableSession({ location, imageName });
   if (!session.hasPreviewImage) uploadScreenshot(imageName);
   return session.url;
