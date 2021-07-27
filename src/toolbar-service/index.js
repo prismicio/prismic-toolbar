20c26a2bd203be40f422ea9ebbda5a4ced8a2a95
lihbr <lihbr@users.noreply.github.com>
2021-07-27 17:16:05 +0200

    fix: improve setup port warning

--- src/toolbar-service/index.js
+++ src/toolbar-service/index.js
@@ -45,18 +45,47 @@ const Client = {
 };
 
 const Iframe = {
+  // If initialization has been delayed
+  initializationDelayed: false,
+  // Warning messages to be shown upon delayed initialization
+  initializationDelayedTimeouts: [],
+  // If iframe has been initialized
+  initialized: false,
+
   async setup() /* void */ {
+    // Reset initialization state (iframe should only be setup once though)
+    Iframe.initializationDelayed = false;
+    Iframe.initializationDelayedTimeouts = [];
+    Iframe.initialized = false;
+
     window.addEventListener('message', msg => Iframe.initialisationMessageHandler(msg));
   },
 
   initialisationMessageHandler(/* MessageEvent */message) /* void */ {
     if (message.data === ToolbarServiceProtocol.SetupPort) {
+      // Checking for `initialized` has a quicker effect than removing the listener because we can check for it after
+      Iframe.initialized = true;
+
       window.removeEventListener('message', msg => Iframe.initialisationMessageHandler(msg));
       const portToMainWindow = message.ports[0];
       setupIframe(portToMainWindow);
       portToMainWindow.postMessage(ToolbarServiceProtocol.Ready);
-    } else {
-      throw Error(`Unexpected message received by the iframe: ${message}.\n Expected ${ToolbarServiceProtocol.SetupPort}`);
+
+      // Clear all ongoing timeouts
+      Iframe.initializationDelayedTimeouts.forEach(timeout => clearTimeout(timeout));
+
+      // If iframe has been delayed, let the user know that the toolbar managed to get ready
+      if (Iframe.initializationDelayed) {
+      // eslint-disable-next-line no-console
+        console.info('%cPrismic toolbar initialized successfully! This message only appears when unexpected messages were received by the iframe during Prismic toolbar setup.\n', 'color: #52b256;');
+      }
+    } else if (!Iframe.initialized) {
+      // Setting a timeout allows to buffer first few messages the iframe might receive when waiting for its own init message
+      Iframe.initializationDelayedTimeouts.push(setTimeout(() => {
+        // If timeout is reached, then iframe had been delayed and may not be initialized
+        Iframe.initializationDelayed = true;
+        console.warn(`Unexpected message received by the iframe during Prismic toolbar setup.\n\nExpected: ${ToolbarServiceProtocol.SetupPort}\nReceived: ${typeof message.data === 'string' ? message.data : JSON.stringify(message.data)}\n\nThis can happen due to an extension tampering with iframes (Dashlane, MetaMask, etc.)\n\nAn explicit message following this one will let you know if the toolbar was successfully initialized.`);
+      }, 500));
     }
   }
 };
