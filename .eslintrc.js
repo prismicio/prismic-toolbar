f128c0165d457fc44fc13a9aa8aad473c15f38b7
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-05-06 13:27:25 -1000

    chore: add dev packages and config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,23 @@
-const path = require('path');
-
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['standard', 'standard-preact', 'airbnb-base'],
-  env: {
-    browser: true,
-    jest: true,
-  },
-  rules: {
-    'no-unused-expressions': 0,
-    'prefer-template': 0,
-    'no-confusing-arrow': 0,
-    'import/no-cycle': 0, // maybe revert this one (config-experiment, panel-children)
-    'no-await-in-loop': 0,
-    'promise/param-names': 0,
-    'class-methods-use-this': 0,
-    'func-names': 0,
-    "global-require": 0,
-    'no-new': 0,
-    'no-multi-assign': 0,
-    'space-before-function-paren': 0,
-    'function-paren-newline': 0,
-    'no-param-reassign': 0,
-    'no-return-assign': [2, 'except-parens'],
-    'no-use-before-define': 0,
-    'consistent-return': 0,
-    'no-prototype-builtins': 0,
-    'arrow-parens': [2, 'as-needed'],
-    'no-console': [2, { allow: ['warn', 'error'] }],
-    'prefer-rest-params': 0,
-    'nonblock-statement-body-position': 0,
-    'no-underscore-dangle': 0,
-    'react/react-in-jsx-scope': 0,
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'only-multiline',
-        objects: 'only-multiline',
-        imports: 'only-multiline',
-        exports: 'only-multiline',
-        functions: 'ignore',
-      },
-    ],
-    'import/prefer-default-export': 0,
-    'import/no-extraneous-dependencies': 0,
-    'object-curly-newline': 0,
-    'implicit-arrow-linebreak': 0,
-    curly: 0,
-  },
-  "settings": {
-    "import/resolver": {
-      webpack: {
-        config: 'webpack.config.js'
-      }
-    }
-  }
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:prettier/recommended",
+		"plugin:react/recommended",
+	],
+	rules: {
+		"no-console": ["warn", { allow: ["info", "warn", "error"] }],
+		"no-debugger": "warn",
+		"no-undef": "off",
+		curly: "error",
+		"prefer-const": "error",
+		"padding-line-between-statements": [
+			"error",
+			{ blankLine: "always", prev: "*", next: "return" },
+		],
+	},
 };
